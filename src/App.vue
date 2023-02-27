194172ba7882dfbb0fee03f6b3c50c2d118f592b
Ayasono <yuehu@ualberta.ca>
2023-02-27 20:08:59 +0800

    Add Some comments

--- src/App.vue
+++ src/App.vue
@@ -34,14 +34,16 @@ export default defineComponent({
 -->
 <template>
 	<h3>vit list</h3>
-	<div class="list" ref="listRef" @scroll.passive="getScroll($event)">
+	<div class='list'
+	     @scroll.passive='getScroll($event)'>
 		<!-- 为了方便我把每一项的高度写死40，展示的条数十条 -->
 		<div :style="{ height: 40 * options.length + 'px', width: '100%' }">
+			<!--			手动设置外部可以容纳所有 list items 的高度-->
 			<div
-				class="item"
-				v-for="(item, index) in options.slice(min - 2, min + 10)"
-				:key="index"
-				:style="{ top: `${40 * item.label}px` }"
+				class='item'
+				v-for='(item, index) in options.slice(min - 2, min + 10)'
+				:key='index'
+				:style='{ top: `${40 * item.label}px` }'
 			>
 				{{ item.value }}:{{ item.label }}
 			</div>
@@ -49,9 +51,9 @@ export default defineComponent({
 	</div>
 </template>
 
+<script lang='ts'>
+import {ref} from 'vue'
 
-<script lang="ts">
-import { ref } from 'vue'
 export default {
 	setup() {
 		let min = ref(2)
@@ -68,7 +70,7 @@ export default {
 		return {
 			// 构造十万条数据
 			options: ref(
-				Array.from({ length: 1000000 }).map((_, idx) => ({
+				Array.from({length: 1000000}).map((_, idx) => ({
 					value: `Option ${idx + 1}`,
 					label: `${idx}`
 				}))
@@ -80,14 +82,17 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang='scss'
+       scoped>
 .list {
 	margin: 10px auto;
 	width: 300px;
 	height: 200px;
 	overflow: auto;
 	position: relative;
+
 	.item {
+		border: 1px solid #be1e3e;
 		height: 40px;
 		line-height: 40px;
 		position: absolute;
